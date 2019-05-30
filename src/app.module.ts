c9de890506f1e76c15651fac0fe194fe89bc91d3
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-30 16:53:17 -0600

    add ShoppingCart controller to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemService } from './items/services/item.service';
+import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ItemsController],
+  controllers: [AppController, ItemsController, ShoppingCartController],
   providers: [AppService, ItemService],
 })
 export class AppModule {}
