4a3bfe6d1de44b4376f4cb340ba2149e8aaefee6
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-30 18:10:46 -0600

    configure Authentication middleware to the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 import { ItemsController } from './items/items.controller';
 import { ItemService } from './items/services/item.service';
+import { AuthenticationMiddleware } from './shared/authentication.middleware';
 import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
-import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [ConfigModule],
   controllers: [AppController, ItemsController, ShoppingCartController],
   providers: [AppService, ItemService],
 })
-export class AppModule {}
+export class AppModule {
+  public configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(
+        { path: '/items', method: RequestMethod.POST },
+        { path: '/shopping-cart', method: RequestMethod.POST },
+      );
+  }
+}
